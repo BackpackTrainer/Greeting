90d73a67a09b963c921099af00d5b62432458742
BackpackTrainer <bill@fairfieldco.com>
2025-06-14 01:57:24 -0400

    fixed problem of database populating on startup, which resulted in duplicate database entries.  Change made the schema.sql file.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS greeting;
+
 CREATE TABLE greeting (
                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          name VARCHAR(255),
+                          name VARCHAR(255) UNIQUE,
                           message VARCHAR(255)
 );
